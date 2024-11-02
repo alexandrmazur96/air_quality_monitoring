1235e8b3b8898b36cd495f8eb5e7950d486f3fef
Olexandr Mazur <alexandrmazur96@gmail.com>
2024-11-02 16:21:52 +0200

    Setup vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+
+import {createApp} from 'vue/dist/vue.esm-bundler';
+import PrimeVue from 'primevue/config';
+import Material from '@primevue/themes/material';
+import AirQualityMap from "./vue/pages/AirQualityMap.vue";
+
+const appOpts = {
+    components: {
+        AirQualityMap,
+    }
+};
+
+const primeVueOpts = {
+    theme: {
+        preset: Material,
+        options: {
+            darkModeSelector: 'system',
+        }
+    }
+};
+
+createApp(appOpts)
+    .use(PrimeVue, primeVueOpts)
+    .mount("#app");
