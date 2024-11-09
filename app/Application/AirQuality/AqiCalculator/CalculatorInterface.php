b92db326efc5ad36bb6fefd90fa20775a1d620a0
Olexandr Mazur <alexandrmazur96@gmail.com>
2024-11-09 23:16:35 +0200

    Added throws to CalculatorInterface

--- app/Application/AirQuality/AqiCalculator/CalculatorInterface.php
+++ app/Application/AirQuality/AqiCalculator/CalculatorInterface.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Application\AirQuality\AqiCalculator;
+namespace Mazur\Application\AirQuality\AqiCalculator;
 
 use Mazur\Application\AirQuality\ApiIntegrations\Dto\AirQuality;
+use Mazur\Application\AirQuality\AqiCalculator\Exceptions\PollutantNotFoundException;
 
 interface CalculatorInterface
 {
+    /** @throws PollutantNotFoundException */
     public function calculate(AirQuality $airQuality): int;
 }
