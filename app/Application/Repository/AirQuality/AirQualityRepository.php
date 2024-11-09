e4d9056011c3a03fc5fdcf83f7a348156221438c
Olexandr Mazur <alexandrmazur96@gmail.com>
2024-11-09 23:16:12 +0200

    Calculate new indexes before insert

--- app/Application/Repository/AirQuality/AirQualityRepository.php
+++ app/Application/Repository/AirQuality/AirQualityRepository.php
@@ -6,17 +6,29 @@ namespace Mazur\Application\Repository\AirQuality;
 
 use Illuminate\Support\Facades\DB;
 use Mazur\Application\AirQuality\ApiIntegrations\Dto\AirQuality;
+use Mazur\Application\AirQuality\AqiCalculator\AqiCalculator;
+use Mazur\Application\AirQuality\AqiCalculator\Enums\AqiType;
 
 final class AirQualityRepository
 {
+    public function __construct(private AqiCalculator $aqiCalculator)
+    {
+    }
+
     /** @param iterable<array-key, AirQuality> $airQualityRecords */
     public function create(iterable $airQualityRecords): void
     {
-        DB::transaction(static function () use ($airQualityRecords): void {
+        DB::transaction(function () use ($airQualityRecords): void {
             foreach ($airQualityRecords as $airQualityRecord) {
-                DB::table('air_quality')->insert([
+                $aqiUk = $this->aqiCalculator->calculate($airQualityRecord, AqiType::UK);
+                $aqiUs = $this->aqiCalculator->calculate($airQualityRecord, AqiType::US);
+                $aqiEurope = $this->aqiCalculator->calculate($airQualityRecord, AqiType::EUROPE);
+                $now = now();
+                DB::table('air_quality_records')->insert([
                     'city_id' => $airQualityRecord->cityId,
-                    'aqi' => $airQualityRecord->aqi,
+                    'aqi_uk' => $aqiUk,
+                    'aqi_us' => $aqiUs,
+                    'aqi_eu' => $aqiEurope,
                     'co' => $airQualityRecord->co,
                     'no' => $airQualityRecord->no,
                     'no2' => $airQualityRecord->no2,
@@ -25,6 +37,8 @@ final class AirQualityRepository
                     'pm2_5' => $airQualityRecord->pm2_5,
                     'pm10' => $airQualityRecord->pm10,
                     'nh3' => $airQualityRecord->nh3,
+                    'created_at' => $now,
+                    'updated_at' => $now,
                 ]);
             }
         });
