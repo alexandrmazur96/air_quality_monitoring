c647ea590f088f6a95063c10ff275ad00b1115fe
Olexandr Mazur <alexandrmazur96@gmail.com>
2024-11-09 23:16:25 +0200

    Removed China aqi index (we willn't calculate it)

--- app/Application/AirQuality/AqiCalculator/Enums/AqiType.php
+++ app/Application/AirQuality/AqiCalculator/Enums/AqiType.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-namespace App\Application\AirQuality\AqiCalculator\Enums;
+namespace Mazur\Application\AirQuality\AqiCalculator\Enums;
 
 enum AqiType: string
 {
     case UK = 'uk';
     case EUROPE = 'europe';
     case US = 'us';
-    case CHINA = 'china';
 }
