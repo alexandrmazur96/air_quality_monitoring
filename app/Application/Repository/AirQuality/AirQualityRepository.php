2fdd791b73a407b96ca9d0722f1b627fb9b4c04e
Olexandr Mazur <alexandrmazur96@gmail.com>
2024-11-16 15:15:23 +0200

    Added markCurrentRecordsAsNonLatest() method, filled new fields when inserting records

--- app/Application/Repository/AirQuality/AirQualityRepository.php
+++ app/Application/Repository/AirQuality/AirQualityRepository.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Mazur\Application\Repository\AirQuality;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Mazur\Application\AirQuality\ApiIntegrations\Dto\AirQuality;
+use Mazur\Application\AirQuality\ApiIntegrations\Enum\Provider;
 use Mazur\Application\AirQuality\AqiCalculator\AqiCalculator;
 use Mazur\Application\AirQuality\AqiCalculator\Enums\AqiType;
 
@@ -15,16 +17,24 @@ final class AirQualityRepository
     {
     }
 
-    /** @param iterable<array-key, AirQuality> $airQualityRecords */
-    public function create(iterable $airQualityRecords): void
+    public function markCurrentRecordsAsNonLatest(Provider $provider): void
     {
-        DB::transaction(function () use ($airQualityRecords): void {
+        DB::table('air_quality_records')
+            ->where('provider', '=', $provider->value)
+            ->update(['latest' => false]);
+    }
+
+    /** @param iterable<array-key, AirQuality> $airQualityRecords */
+    public function create(Provider $provider, iterable $airQualityRecords): void
+    {
+        DB::transaction(function () use ($airQualityRecords, $provider): void {
             foreach ($airQualityRecords as $airQualityRecord) {
                 $aqiUk = $this->aqiCalculator->calculate($airQualityRecord, AqiType::UK);
                 $aqiUs = $this->aqiCalculator->calculate($airQualityRecord, AqiType::US);
                 $aqiEurope = $this->aqiCalculator->calculate($airQualityRecord, AqiType::EUROPE);
                 $now = now();
                 DB::table('air_quality_records')->insert([
+                    'provider' => $provider->value,
                     'city_id' => $airQualityRecord->cityId,
                     'aqi_uk' => $aqiUk,
                     'aqi_us' => $aqiUs,
@@ -39,8 +49,19 @@ final class AirQualityRepository
                     'nh3' => $airQualityRecord->nh3,
                     'created_at' => $now,
                     'updated_at' => $now,
+                    'latest' => true,
                 ]);
             }
         });
+    }
+
+    /** @return Collection<array-key, object{provider:string, latitude: float, longitude: float, aqi_uk: int, aqi_us: int, aqi_eu: int} */
+    public function getCurrentAirQualityIndexes(): Collection
+    {
+        return DB::table('air_quality_records')
+            ->join('cities', 'air_quality_records.city_id', '=', 'cities.id')
+            ->where('air_quality_records.latest', '=', true)
+            ->select('provider', 'cities.latitude', 'cities.longitude', 'aqi_uk', 'aqi_us', 'aqi_eu')
+            ->get();
     }
 }
